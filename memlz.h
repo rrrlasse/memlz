e1f9f3f7f3b7d50fbe6738133e33b953e9ed4f44
rrrlasse <exdupe@gmail.com>
2025-11-01 20:34:25 +0100

    Increase compression ratio by 1-2%

--- memlz.h
+++ memlz.h
@@ -72,8 +72,8 @@ static void memlz_reset(memlz_state* c);
 #define MEMLZ_DO_RLE
 #define MEMLZ_DO_INCOMPRESSIBLE
 #define MEMLZ_INCOMPRESSIBLE (32 * sizeof(uint64_t))
-#define MEMLZ_PROBELEN (16 * 1024)
-#define MEMLZ_BLOCKLEN (256 * 1024)
+#define MEMLZ_PROBELEN (2 * 1024)
+#define MEMLZ_BLOCKLEN (128 * 1024)
 #define MEMLZ_RLE 'D'
 #define MEMLZ_MIN_RLE (4 * sizeof(uint64_t))
 
